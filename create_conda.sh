9bef97bb7fb755df4f96a70552972d0f4aaf5c6d
Joscha Rieber <joscha.rieber@iais.fraunhofer.de>
2020-08-11 14:45:48 +0200

    Updated librosa in conda environment definition

--- create_conda.sh
+++ create_conda.sh
@@ -12,9 +12,7 @@ fi
 
 bash Miniconda3-latest-Linux-x86_64.sh -u -b -p conda
 conda/bin/conda info
-conda/bin/conda install -y -c anaconda numpy=1.18.5
-conda/bin/conda install -y -c conda-forge librosa=0.7.2
+conda/bin/conda install -y -c conda-forge librosa=0.8.0
 conda/bin/conda install -y -c anaconda tensorflow-gpu=2.2.0
 conda/bin/conda install -y -c conda-forge imageio=2.8.0
 conda/bin/conda install -y -c conda-forge jupyterlab=2.1.5
-conda/bin/conda install -y -c anaconda numba=0.48
